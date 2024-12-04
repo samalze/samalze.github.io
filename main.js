5d742e0a4c5eaf0913a202c2dd3443a2ada44d16
samalze <groupilanddu33@gmail.com>
2024-12-04 23:16:41 +0100

    début

---
+++ main.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', () => {
+    // Sélectionne l'élément avec la classe "banderole"
+    const banderole = document.querySelector('.banderole');
+    const titre = document.querySelector('.titre_banderole');
+    const lien = document.querySelector('.lien_banderole');
+    // Vérifie que l'élément existe avant de continuer
+    if (!banderole) {
+        console.error("L'élément avec la classe 'banderole' est introuvable !");
+        return;
+    }
+
+    // Ajoute l'événement de défilement
+    document.addEventListener('scroll', () => {
+        if (window.scrollY > 150) {
+            banderole.classList.add('transparent'); // Ajoute la classe "transparent"
+            titre.classList.add('taille'); // Ajoute la classe "transparent"
+            lien.classList.add('taille1'); // Ajoute la classe "transparent"
+        } else {
+            banderole.classList.remove('transparent'); // Retire la classe "transparent"
+            titre.classList.remove('taille'); // Retire la classe "transparent"
+            lien.classList.remove('taille'); // Retire la classe "transparent"
+
+        }
+    });
+});
